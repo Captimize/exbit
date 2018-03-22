d5183b3b5fd55765cf39a9bd5990314ef27b6ad0
bit <bit@DESKTOP-SL51PLF>
2018-03-22 14:35:42 +0900

    add_hotfix_00

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,6 +3,12 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("1_hello_git");
+		//1. hotfix 추가 후 마스터 브렌치 수정
 		
+		System.out.println("2_add_hotfix");
+		//2. m
+		
+		System.out.println("3_add master");
+	
 	}
 }
